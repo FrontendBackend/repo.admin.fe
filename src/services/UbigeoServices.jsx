d72583371b61ad416683e16236ae4c6af3bfb78e
juanvalerio <juanvalerio785@gmail.com>
2025-08-30 20:50:37 -0500

    configurar normalizacion del texto para la busqueda del ubigeo

--- src/services/UbigeoServices.jsx
+++ src/services/UbigeoServices.jsx
@@ -1,8 +1,17 @@
 import api from "../config/axiosConfig";
 
 export const filtrarUbigeo = async (filtro) => {
+  const filtroNormalizado = normalizarTexto(filtro);
   const res = await api.get(
-    `/ubigeos/buscar?filtro=${encodeURIComponent(filtro)}`
+    `/ubigeos/buscar?filtro=${encodeURIComponent(filtroNormalizado)}`
   );
   return res.data;
 };
+
+function normalizarTexto(texto) {
+  return texto
+    .normalize("NFD") // separa letras y tildes
+    .replace(/[\u0300-\u036f]/g, "") // elimina tildes
+    .replace(/ñ/gi, "n") // reemplaza ñ -> n
+    .toUpperCase(); // opcional: mayúsculas
+}
